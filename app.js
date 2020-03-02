c7ecafea7591e069ba27cf78e3e61aa439c3f0ed
LIMBYEONGCHAE <ithing108@gmail.com>
2020-03-03 02:19:41 +0900

    set router

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
 // const express =  require('express');
 import express from "express";
 import morgan from "morgan";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";    
-import { userRouter } from "./router";
+import { userRouter } from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
 const PORT = 4000;
 
-function handleListening(){
-    console.log(`listen on :port:${PORT}`);
-}
-
-const handleHome = (req, res) => res.send("hello from home");    
-
-const handleProfile = (req, res) => res.send("you are on my profile");
-
-const betweenHome = (req, res, next) => {       // middleware - file transfer , log, ip check
-    console.log("i'm between");
-    // res.send("not happening");               // middleware로 연결을 중단할 수 있다.
-    next();
-}
+app.set('view engine', 'pug');
 app.use(cookieParser());                        // use middleware - to every route
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("dev"));                         // app.use(helmet())
+app.use(helmet());
+app.use(morgan("dev"));                         // log
                                                 // route가 먼저  실행되면 적용되지 않는다.
 
-app.get("/", betweenHome, handleHome);          // use middleware - to one
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);                   // user/~~
-
+app.use(routes.home, globalRouter);                                                
+app.use(routes.users, userRouter);              // user/me
+app.use(routes.videos, videoRouter);
 export default app;                             // const app을 주겠다.
+
+
+
+/*
+function handleHome(){
+    console.log(`listen on :port:${PORT}`);
+}
+const betweenHome = (req, res, next) => {      // middleware - file transfer , log, ip check
+    console.log("i'm between");
+     res.send("not happening");                // middleware로 연결을 중단할 수 있다.
+    next();
+}
+app.get("/", betweenHome, handleHome);         // use middleware - to one
+
+
+const handleProfile = (req, res) => res.send("you are on my profile");
+app.get("/profile", handleProfile); */
